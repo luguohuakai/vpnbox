a7e5caca4d0dbd8d8c0a0c7b234d62d8d8c7fc13
DM <769245396@qq.com>
2017-12-13 16:07:31 +0800

    (SS -> LS) 使用LocalStorage存储user_name token

--- public/include/proxy.php
+++ public/include/proxy.php
@@ -10,7 +10,7 @@
 //$content = file_get_contents($interface);
 //exit($content);
 include_once './function.php';
-sleep(1);
+//sleep(1);
 //$host_8080 = 'https://121.43.167.65:8080/api/';
 $host_8080 = 'https://wx.srun.com:8080/api/';
 //$host_8080 = 'https://47.104.1.91:8080/api/';
